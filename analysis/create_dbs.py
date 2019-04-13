55d8a44989439853595ded35fcf0ab15657e9012
madewithsmiles <jndiarrassouba@gmail.com>
2019-04-13 17:37:31 -0500

    Changed some shit lol

---
+++ analysis/create_dbs.py
@@ -0,0 +1,6 @@
+import sqlalchemy as db
+from tables import State, Tornado, Industry, Gdp
+
+# engine = db.create_engine('sqlite:///data.db', echo=True)
+# connection = engine.connect()
+# # engine
---
+++ analysis/tables.py
@@ -0,0 +1,42 @@
+from sqlalchemy import Column, INTEGER, CHAR, FLOAT, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+# -------------------------------- State #
+class State(Base):
+    __tablename__ = 'state'
+
+    StateID = Column(INTEGER, primary_key=True, nullable=False)
+    StateName = Column(CHAR(50), nullable=False)
+
+# -------------------------------- Tornado Info #
+class Tornado(Base):
+    __tablename__ = 'tornado'
+
+    TornadoID = Column(INTEGER, primary_key=True, nullable=False)
+    Yr = Column(INTEGER, nullable=False)
+    Mo = Column(INTEGER, nullable=False)
+    Dy = Column(INTEGER, nullable=False)
+    Magnitude = Column(INTEGER, nullable=False)
+    Latitude = Column(FLOAT, nullable=False)
+    Longitude = Column(FLOAT, nullable=False)
+    StateID = Column(INTEGER, ForeignKey("state.StateID"), nullable=False)
+
+
+# -------------------------------- Industry #
+class Industry(Base):
+    __tablename__ = 'industry'
+
+    IndustryID = Column(INTEGER, primary_key=True, nullable=False)
+    IndustryName = Column(CHAR(50), nullable=False)
+
+# -------------------------------- GDP #
+class Gdp(Base):
+    __tablename__ = 'gdp'
+
+    GdpID = Column(INTEGER, primary_key=True,nullable=False)
+    Yr = Column(INTEGER, nullable=False)
+    StateID = Column(INTEGER, ForeignKey("state.StateID"), nullable=False)
+    IndustryID = Column(INTEGER, ForeignKey("industry.IndustryID") ,nullable=False)
+    
